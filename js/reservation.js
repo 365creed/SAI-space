bbe26c03fa6bf305f335a7e9b8ee1f742a7a0763
Yeongu <46391818+365creed@users.noreply.github.com>
2026-03-16 22:58:04 +0900

    Update reservation.js

--- js/reservation.js
+++ js/reservation.js
@@ -1,1 +1,37 @@
+const form = document.querySelector("#reservationForm");
 
+if(form){
+
+form.addEventListener("submit", async (e)=>{
+
+e.preventDefault();
+
+const data = {
+
+name:form.name.value,
+phone:form.phone.value,
+space:form.space.value,
+date:form.date.value,
+time:form.time.value
+
+};
+
+
+if(!data.date || !data.time){
+
+alert("날짜와 시간을 선택하세요");
+
+return;
+
+}
+
+
+await sendReservation(data);
+
+alert("예약 요청이 접수되었습니다.");
+
+form.reset();
+
+});
+
+}
