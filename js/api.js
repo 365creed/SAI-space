49f84aa9b1f18893e6d880aa5312e316ab25d770
Yeongu <46391818+365creed@users.noreply.github.com>
2026-03-16 22:57:53 +0900

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,26 @@
+const API_URL = "YOUR_GOOGLE_SCRIPT_URL";
 
+
+async function sendReservation(data){
+
+try{
+
+const res = await fetch(API_URL,{
+
+method:"POST",
+body:JSON.stringify(data),
+headers:{
+"Content-Type":"application/json"
+}
+
+});
+
+return await res.json();
+
+}catch(e){
+
+console.error(e);
+
+}
+
+}
